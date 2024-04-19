5c0afce053c2996100299fcba95c4d9194c0d3b8
kafemarketing <kafemarketing@gmail.com>
2024-04-19 14:16:32 -0500

    updates

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,12 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://azureresumefuncapp.azurewebsites.net/api/GetResumeCounter?code=65lXymWvpVoqQ8UxaOrA9BgBdyDJXP8PORuBG0IabzAQAzFuFBYGYA=='
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
